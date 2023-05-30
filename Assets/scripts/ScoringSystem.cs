955b8041c60e829c1cbcf95bc44e758071dcfc54
NicoleHielt <nhieltjes@gmail.com>
2023-05-30 10:06:57 +0200

    chore
    ignore

---
+++ Assets/scripts/ScoringSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    int currentScore = 0;
+    [SerializeField] Text ScoreText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PointCollider")
+        {
+            currentScore++;
+            Debug.Log(currentScore);
+        }
+    }
+
+    private void Update()
+    {
+        ScoreText.GetComponent<Text>();
+        ScoreText.text = "Score: " + currentScore.ToString();
+    }
+}
