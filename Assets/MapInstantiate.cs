3924ac13c02213efa283b12357576c8442047cbe
Jort-Altena <0344911@student.rocvantwente.nl>
2023-05-30 09:29:53 +0200

    feat: Print weg gehaald uit MapScript

--- Assets/MapInstantiate.cs
+++ Assets/MapInstantiate.cs
@@ -8,11 +8,9 @@ public class MapInstantiate : MonoBehaviour
     [SerializeField] GameObject mapTwo;
     [SerializeField] GameObject mapThree;
     [SerializeField] int mapLocation;
+
     float locationChanger = 0;
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
@@ -22,21 +20,18 @@ public class MapInstantiate : MonoBehaviour
            Vector2 mapPosition = new Vector2(locationChanger, 0.0f);
            locationChanger = locationChanger + mapLocation;
             Instantiate(map, mapPosition, Quaternion.identity);
-            print("gwgw");
         }
         else if (Input.GetKeyDown(","))
         {
             Vector2 mapPosition = new Vector2(locationChanger, 0.0f);
             locationChanger = locationChanger + mapLocation;
             Instantiate(mapTwo, mapPosition, Quaternion.identity);
-            print("gwgw");
         }
         else if (Input.GetKeyDown("."))
         {
             Vector2 mapPosition = new Vector2(locationChanger, 0.0f);
             locationChanger = locationChanger + mapLocation;
             Instantiate(mapThree, mapPosition, Quaternion.identity);
-            print("gwgw");
         }
 
     }
