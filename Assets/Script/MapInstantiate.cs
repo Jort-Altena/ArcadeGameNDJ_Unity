65fa012629977ec20d88b83258d5cfdd79210e28
AsianScammer <dennis29012006@gmail.com>
2023-05-31 09:34:20 +0200

    Fix:Map initiate, map changes.
    Map initiate is van script aangepast. Het spawnt nu 1x 10x
    
    Map is veranderd omdat de foto kleiner moest

---
+++ Assets/Script/MapInstantiate.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapInstantiate : MonoBehaviour
+{
+    [SerializeField] GameObject map;
+    [SerializeField] GameObject mapTwo;
+    [SerializeField] GameObject mapThree;
+    [SerializeField] int mapLocation;
+    float locationChanger = 0;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            int randomNumber = Random.Range(1, 4);
+            switch (randomNumber)
+            {
+                case 1:
+                    Vector2 mapPosition = new Vector2(locationChanger, 0.0f);
+                    locationChanger = locationChanger + mapLocation;
+                    Instantiate(map, mapPosition, Quaternion.identity);
+                    break;
+
+                case 2:
+                    mapPosition = new Vector2(locationChanger, 0.0f);
+                    locationChanger = locationChanger + mapLocation;
+                    Instantiate(mapTwo, mapPosition, Quaternion.identity);
+                    break;
+                case 3:
+                    mapPosition = new Vector2(locationChanger, 0.0f);
+                    locationChanger = locationChanger + mapLocation;
+                    Instantiate(mapThree, mapPosition, Quaternion.identity);
+                    break;
+
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+       
+    }
+}
